53ef1b0795075a5c738071fede8368e0fdca9ad2
Martin Pedersen <42993919+MartinLieb@users.noreply.github.com>
2019-03-20 11:40:17 +0100

    FIXED

--- app/src/main/java/dk/acsandras/mvvmdemo/viewmodel/ModelViewModel.java
+++ app/src/main/java/dk/acsandras/mvvmdemo/viewmodel/ModelViewModel.java
@@ -1,33 +1,43 @@
 package dk.acsandras.mvvmdemo.viewmodel;
 
 
+import java.util.Observable;
+import java.util.Observer;
+
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 import dk.acsandras.mvvmdemo.model.Model;
 
-public class ModelViewModel extends ViewModel {
+public class ModelViewModel extends ViewModel implements Observer {
 
     // TODO (4) ViewModel skal også bruge LiveData, som de forskellige activities kan observere
     // ViewModel er også løst koblet på modellen, dvs. modellen har ikke kendskab til ViewModel
     private MutableLiveData<String> currentA = new MutableLiveData<>();
     private Model model = new Model();
 
+
     public ModelViewModel() {
         // TODO (5) Det her illustrerer, at vi får data fra modellen
-        currentA.setValue(model.getA().getValue()); // Giver "Startdata fra Model"
+        currentA.setValue(model.getA()); // Giver "Startdata fra Model"
         // currentA.setValue("Startdata fra ViewModel");
+        model.addObserver(this);
 
     }
 
     public MutableLiveData<String> getA() {
         // TODO (6) Hent data fra modellen
-        currentA = model.getA();
         return currentA;
     }
 
     public void setA(String a) {
         // TODO (7) Skriv data til modellen
         model.setA(a);
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        currentA.setValue(model.getA());
+
     }
 
 }
